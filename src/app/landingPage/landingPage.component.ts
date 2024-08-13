19dce8fabf679b88070d8824c68d722decec27c6
ThomasJilge <thomasjilge@gmx.de>
2024-08-13 21:42:57 +0200

    folder structure

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from '../main-content/landing-page/navbar/navbar.component';
 
 @Component({
     selector: 'app-landingPage',
