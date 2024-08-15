1e9295407dbf50e6c1f4929bd6a24a028176e650
ThomasJilge <thomasjilge@gmx.de>
2024-08-15 11:35:16 +0200

    html sakura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first-component', component: Firstcomponent },
+    { path: 'secont-component', component: SecondComponent},
+];
